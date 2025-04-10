54b6ed825499fe1d3c699b1bcdea7be6eeb6c152
shreya-bandhe-25 <bandheshreya@gmail.com>
2025-04-10 19:06:01 +0530

    Add files via upload
    This project presents a comprehensive analysis of pizza sales data, using SQL to answer essential business questions. The project is structured into three levels of complexity:
    Basic :- Total orders, revenue generation, highest-priced pizza, most common pizza size, and top revenue generating pizza types by quantity.
    Intermediate :- Category-wise and hourly distribution to revenue, cumulative revenue trends, and revenue-based ranking within categories.
    Advance :- Percentage contribution to revenue, cumulative revenue trends, and revenue-based ranking within categories.

---
+++ SQL Project on pizza sales.sql
@@ -0,0 +1,9 @@
+-- Determine the top 3 most ordered pizza types based on revenue.
+
+select pizza_types.name, 
+sum(orders_details.quantity * pizzas.price) as revenue
+from pizza_types join pizzas
+on pizzas.pizza_type_id = pizza_types.pizza_type_id
+join orders_details 
+on orders_details.pizza_id = pizzas.pizza_id
+group by pizza_types.name order by revenue desc limit 3;
